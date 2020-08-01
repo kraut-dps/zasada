c8ea144fe25185d7423530cbe4008d8a044b1811
kraut <sauerkraut@inbox.ru>
2020-08-02 00:14:59 +0300

    fix example webpack "Invalid Host header"

--- examples/1_HelloWorld/webpack.config.js
+++ examples/1_HelloWorld/webpack.config.js
@@ -42,12 +42,14 @@ module.exports = [
 			]
 		},
 		devServer: {
-			contentBase: __dirname,
+			contentBase: __dirname + '/',
 			open: true, // open browser
 			overlay: true, // display error overlay
 			hot: true, // enable hot module replacement
 			disableHostCheck: true,
+			inline: true, // for HMR
 			stats: "errors-only", // display only errors to reduce the amount of output
+			watchContentBase: true, // HMR html
 		},
 		resolve: {
 			modules: ["node_modules"]
@@ -55,5 +57,5 @@ module.exports = [
 		plugins: [
 			new webpack.HotModuleReplacementPlugin(),
 		]
-	}
+	},
 ];
