da3839e4833901841bdc484e9648fa855d6f0ded
kraut <sauerkraut@inbox.ru>
2020-08-01 23:35:15 +0300

    fix example webpack "Invalid Host header"

--- examples/1_HelloWorld/webpack.config.js
+++ examples/1_HelloWorld/webpack.config.js
@@ -1,51 +1,58 @@
+var webpack = require( 'webpack' );
 module.exports = [
-{
-  mode: 'none',
-  devtool: 'source-map',
-  entry: {
-    index: __dirname + '/index.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: __dirname + '/build/',
-    publicPath: '/build/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  "loose": false,
-                  "targets": {
-                    "ie": "9"
-                  }
-                }
-              ]
-            ],
-            plugins: [
-              "@babel/plugin-proposal-class-properties",
-              "@babel/plugin-proposal-function-bind",
-              "@babel/plugin-proposal-object-rest-spread",
-              "@babel/plugin-syntax-dynamic-import"
-            ]
-          }
-        },
-        //exclude: /node_modules/
-      },
-    ]
-  },
-  devServer: {
-    contentBase: __dirname,
-    disableHostCheck: true
-  },
-  resolve: {
-    modules: ["node_modules"]
-  },
-  plugins: []
-} ];
+	{
+		mode: 'none',
+		devtool: 'source-map',
+		entry: {
+			index: __dirname + '/index.js'
+		},
+		output: {
+			filename: '[name].js',
+			path: __dirname + '/build/',
+			publicPath: '/build/',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								[
+									"@babel/preset-env",
+									{
+										"loose": false,
+										"targets": {
+											"ie": "9"
+										}
+									}
+								]
+							],
+							plugins: [
+								"@babel/plugin-proposal-class-properties",
+								"@babel/plugin-proposal-function-bind",
+								"@babel/plugin-proposal-object-rest-spread",
+								"@babel/plugin-syntax-dynamic-import"
+							]
+						}
+					},
+					//exclude: /node_modules/
+				},
+			]
+		},
+		devServer: {
+			contentBase: __dirname,
+			open: true, // open browser
+			overlay: true, // display error overlay
+			hot: true, // enable hot module replacement
+			stats: "errors-only", // display only errors to reduce the amount of output
+		},
+		resolve: {
+			modules: ["node_modules"]
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+		]
+	}
+];
